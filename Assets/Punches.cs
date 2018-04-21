28af85baffca1c1b53c13d572b52ba7b872989b3
Unknown <jakubeklech@o2.pl>
2018-04-21 20:02:30 +0200

    2rd init

---
+++ Assets/Punches.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punches : MonoBehaviour {
+	[SerializeField] private GameObject pun;
+	private Vector3 mouse;
+	[SerializeField] private Transform Mother;
+	[SerializeField] private GameObject prefab;
+
+	[SerializeField] private Sprite Lewy;
+	[SerializeField] private Sprite Prawy;
+	private GameObject Fist;
+	[SerializeField] private bool prawa = false;
+
+	private float RanX,RanY;
+
+	// Use this for initialization
+	void Start () {
+		Mother = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Follow();
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			Punch();
+		}
+
+	}
+
+	void Follow()
+	{
+		mouse = Input.mousePosition;
+		mouse.z = 10;
+		mouse = Camera.main.ScreenToWorldPoint(mouse);
+		gameObject.transform.position = mouse;
+	}
+
+	void Punch()
+	{	
+		Transform Des = Mother;
+
+		RanX = Random.Range(-4.0f, 4.0f);
+		RanY = Random.Range(-3.0f, -6.0f);
+
+		//create punch
+		//losowo spawniwać rękawice żeby poruszała się w strone myszy
+		 Fist = Instantiate(prefab, new Vector3(RanX,RanY,0), Quaternion.identity);
+		if (prawa == true)
+		{
+			prawa = false;
+			Fist.GetComponent<SpriteRenderer>().sprite = Lewy;
+		}
+		if (prawa == false)
+		{ 
+			Fist.GetComponent<SpriteRenderer>().sprite = Prawy;
+			prawa = true;
+		}
+		
+	
+		float rot_z = Mathf.Atan2(Des.transform.position.y, Des.transform.position.x) * Mathf.Rad2Deg;
+         Fist.transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+		
+
+
+	}
+}
---
+++ Assets/fly.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fly : MonoBehaviour {
+
+	[SerializeField] private float step = 20f;
+	[SerializeField]private Vector3 Tar;
+	[SerializeField]private Vector3 startpos;
+	[SerializeField]private float speed,acc;
+	[SerializeField] Vector2 aha;
+	private Vector2 curPos, lastPos;
+	// Use this for initialization
+	void Start () {
+		startpos = transform.position;
+		Vector3	mouse = Input.mousePosition;
+		mouse.z = 10;
+		mouse = Camera.main.ScreenToWorldPoint(mouse);
+		//gameObject.transform.position = mouse;
+		Tar = mouse;
+	}
+	void Update()
+	{
+	 curPos = transform.position;
+     if(curPos == lastPos) {
+         Boom();
+     }
+     lastPos = curPos;
+	}
+	void FixedUpdate()
+	{
+		step = speed * Time.deltaTime;
+		speed += acc;
+		gameObject.transform.position = Vector3.MoveTowards(transform.position, Tar, step);
+	}
+
+	/// <summary>
+	/// Sent each frame where a collider on another object is touching
+	/// this object's collider (2D physics only).
+	/// </summary>
+	/// <param name="other">The Collision2D data associated with this collision.</param>
+	void OnCollisionStay2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "Punches")
+		{
+			Boom();
+		}
+	}
+
+	void Boom()
+	{
+
+		//screnshake
+		Destroy(gameObject);
+	}
+}
